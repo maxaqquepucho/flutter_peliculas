c1ff12f4d3b0c82d17d58a231af4c06982cf224d
Max Aqquepucho <max.aqquepucho@global66.com>
2021-05-25 20:58:03 -0500

    Add peliculas model al peliculas provider

---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,23 @@
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class PeliculasProvider {
+  String _apiKey = '956790b0ca2df24a4b73320fd53ab6ea';
+  String _url = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+    });
+
+    final resp = await http.get(url);
+    final decodeData = json.decode(resp.body);
+
+    final peliculas = new Peliculas.fromJsonList(decodeData['results']);
+
+    return peliculas.items;
+  }
+}
